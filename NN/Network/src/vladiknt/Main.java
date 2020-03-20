cf20f1bea732560ab94f7ee6a85761842c6a0c30
Vonisak <ivan.kasinov@mail.ru>
2020-03-21 02:45:02 +0300

    create multiply method in Matrix class

--- NN/Network/src/vladiknt/Main.java
+++ NN/Network/src/vladiknt/Main.java
@@ -1,8 +1,22 @@
 package vladiknt;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        double[][] mat1 = new double[][] {
+                {1, 2, 4, 5},
+                {3, 4, 12, 78},
+                {1, 4, 18, 7}
+        };
+        double[][] mat2 = new double[][] {
+                {5, 7, 1, 6},
+                {1, 7, 1, 7},
+                {5, 12, 13, 9},
+                {45, 16, 6, -1}
+        };
 
+        System.out.println(Arrays.deepToString(Matrix.multiply(mat1, mat2)));
     }
 }
--- NN/Network/src/vladiknt/Matrix.java
+++ NN/Network/src/vladiknt/Matrix.java
@@ -6,11 +6,20 @@ public class Matrix {
 //                {1, 2},
 //                {3, 4}
 //        };
-        double[][] result;
-        if(mat1.length == mat2[0].length) {
-            for(int i = 0; i < ){
+        double[][] result = new double[mat1.length][mat2[0].length];
+        double sum;
 
+        if(mat1[0].length == mat2.length) {
+            for(int i = 0; i < mat1.length; i++){
+                for(int j = 0; j < mat2[0].length; j++) {
+                    sum = 0;
+                    for(int k = 0; k < mat1[0].length; k++) {
+                        sum += mat1[i][k] * mat2[k][j];
+                    }
+                    result[i][j] = sum;
+                }
             }
+            return result;
         }
         return null;
     }
