0c42c6da6c5db1a172df08658cb94b725ab7aad9
Vonisak <ivan.kasinov@mail.ru>
2020-03-21 02:11:32 +0300

    create class Matrrix

--- NN/Network/src/vladiknt/Main.java
+++ NN/Network/src/vladiknt/Main.java
@@ -3,11 +3,6 @@ package vladiknt;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        int f = 0;
-        int e = 5;
-        int d = f+e;
-        System.out.println(d);
-        System.out.println("Vanya pidor");
+
     }
 }
---
+++ NN/Network/src/vladiknt/Matrix.java
@@ -0,0 +1,17 @@
+package vladiknt;
+
+public class Matrix {
+    public static double[][] multiply(double[][] mat1, double[][] mat2) {
+//        double[][] arr = {
+//                {1, 2},
+//                {3, 4}
+//        };
+        double[][] result;
+        if(mat1.length == mat2[0].length) {
+            for(int i = 0; i < ){
+
+            }
+        }
+        return null;
+    }
+}
