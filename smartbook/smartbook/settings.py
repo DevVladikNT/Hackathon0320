ce8ce838103558f91aa6488f73e23f262aefc4d0
p-dnice <38218626+p-dnice@users.noreply.github.com>
2020-03-21 17:19:52 +0300

    SSS

--- smartbook/smartbook/settings.py
+++ smartbook/smartbook/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'book.apps.BookConfig',
+    'users.apps.UsersConfig',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- smartbook/smartbook/urls.py
+++ smartbook/smartbook/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('book/', include('book.urls')),
+	path('account/', include('users.urls')),
+	path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
