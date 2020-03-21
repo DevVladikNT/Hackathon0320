85fa390df2dafc6eae8b0a2b72d6ac55f7769a59
Alpa-ch <saintsalien@gmail.com>
2020-03-21 03:47:24 +0300

    Create html+css+js files

---
+++ WebShit/JS/JS_home/special-offer.js
@@ -0,0 +1,10 @@
+/*
+ $(document).ready(function(){
+                $("button").click(function(){
+                    $("img").hide();
+                });
+            });
+*/
+$(document).ready(function() {
+    $(".content").html("Gennady's Test");
+});
